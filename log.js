5eb5a6c66f868634953509cce8c95da9a5105ae6
avatar-lavventura <alper.alimoglu@gmail.com>
2017-12-13 09:43:45 +0300

    more clean-up is done, comments have been fixed for better code.

--- log.js
+++ log.js
@@ -1,9 +1,9 @@
 var nodePaths = require('./nodePaths');
-var mylib = require(nodePaths.EBLOCBROKER + '/eBlocHeader.js');
+var mylib     = require(nodePaths.EBLOCBROKER + '/eBlocHeader.js');
 
-console.log( '' + mylib.getDeployedBlockNumber() )
+console.log('' + mylib.getDeployedBlockNumber())
 
 //readFrom = mylib.getDeployedBlockNumber()
 readFrom = 1382295
-//mylib.LogJob( readFrom, nodePaths.LOG_PATH + '/queuedJobs.txt' ); //COMPLETED olmayanlari print et.
+//mylib.LogJob( readFrom, nodePaths.LOG_PATH + '/queuedJobs.txt' ); /* Prints jobs, which are not COMPLETED */
 mylib.LogReceipt(readFrom, nodePaths.LOG_PATH + '/queuedJobs.txt');
--- startCode.py
+++ startCode.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python
-
 import sys, os, constants, time
 
 def startCall( jobKey, index ):
-   cluster_id = constants.CLUSTER_ID;
-   eblocPath  = constants.EBLOCPATH; 
-   logPath    = constants.LOG_PATH; 
-
+   cluster_id              = constants.CLUSTER_ID;
+   eblocPath               = constants.EBLOCPATH;
+   logPath                 = constants.LOG_PATH;
+   
    os.environ['eblocPath'] = eblocPath;
    os.environ['index']     = str(index);
    os.environ['jobKey']    = jobKey;
-   statusId                = str(constants.job_state_code['RUNNING']); 
-   os.environ['statusId']  = statusId;
-   
+   statusId                = str(constants.job_state_code['RUNNING']);
+   os.environ['statusId']  = statusId;   
    unixTime                = os.popen('date +%s').read();
-   unixTime                = str(int(unixTime) + 1)
-   os.environ['unixTime']  = unixTime
+   unixTime                = str(int(unixTime) + 1);
+   os.environ['unixTime']  = unixTime;
+   
    txHash = os.popen('node $eblocPath/eBlocBrokerNodeCall.js setJobStatus $jobKey $index $statusId $unixTime').read().replace("\n", "").replace(" ", "");
    
    while(True):
       if (not(txHash == "notconnected" or txHash == "")): 
-         break;
+         break;      
       else:
          os.environ['unixTime'] = unixTime;
-         txHash = os.popen('node $eblocPath/eBlocBrokerNodeCall.js setJobStatus $jobKey $index $statusId $unixTime').read().replace("\n", "").replace(" ", "");        
-      time.sleep(5)
+         txHash = os.popen('node $eblocPath/eBlocBrokerNodeCall.js setJobStatus $jobKey $index $statusId $unixTime').read().replace("\n", "").replace(" ", "");
+      time.sleep(5);
       
    txFile = open( logPath + '/transactions/' + cluster_id + '.txt', 'a');
    txFile.write( txHash + " start_setJobStatus" +  " " + unixTime + "\n");
@@ -33,4 +32,4 @@ def startCall( jobKey, index ):
 if __name__ == '__main__': 
    jobKey   = sys.argv[1];
    index    = sys.argv[2];
-   startCall( jobKey, index );
+   startCall(jobKey, index);
