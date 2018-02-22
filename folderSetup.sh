afeca92dbd92581ce141d56e206568dc119e089a
avatar-lavventura <alper.alimoglu@gmail.com>
2018-02-22 15:30:35 +0300

    folderSetup and nameUpdate is combined under initialize.sh

--- folderSetup.sh
+++ folderSetup.sh
@@ -1,16 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir  ~/.eBlocBroker
-cd     ~/.eBlocBroker
-mkdir transactions ipfs_hashes endCodeAnalyse 
+line_old='/alper/'
+line_new='/ubuntu/'
 
-touch  ~/.eBlocBroker/queuedJobs.txt
-touch  ~/.eBlocBroker/transactions/clusterOut.txt
+sed -i "s%$line_old%$line_new%g" nodePaths.js
+sed -i "s%$line_old%$line_new%g" checkSinfo.sh
+sed -i "s%$line_old%$line_new%g" constants.py
+sed -i "s%$line_old%$line_new%g" slurmScript.sh
+sed -i "s%$line_old%$line_new%g" ipfsStat.sh
+sed -i "s%$line_old%$line_new%g" ipfsGet.sh
 
-sudo chmod +x ~/eBlocBroker/slurmScript.sh
 
-#Required after each reboot. Unneeded with the latest update.
-#mkdir ~/.eBlocBroker/oc
-#sudo mount.davfs https://b2drop.eudat.eu/remote.php/webdav/ ~/.eBlocBroker/oc
-#mlck id aalimog1@binghamton.edu --save
-#exfoliation econometrics revivifying obsessions transverse salving dishes
+line_old='0x6af0204187a93710317542d383a1b547fa42e705'
+#line_new=$(echo $COINBASE)
+line_new='0xda1e61e853bb8d63b1426295f59cb45a34425b63'
+
+sed -i "s%$line_old%$line_new%g" constants.py
+sed -i "s%$line_old%$line_new%g" eBlocHeader.js
+sed -i "s%$line_old%$line_new%g" main.js
+
+line_old='alper'
+line_new='ubuntu'
+
+sed -i "s%$line_old%$line_new%g" constants.py
--- initialize.sh
+++ initialize.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Folder Setup:--------------------------------------
+mkdir  $HOME/.eBlocBroker
+cd     $HOME/.eBlocBroker
+mkdir transactions ipfs_hashes endCodeAnalyse 
+
+touch  $HOME/.eBlocBroker/queuedJobs.txt
+touch  $HOME/.eBlocBroker/transactions/clusterOut.txt
+
+sudo chmod +x $HOME/eBlocBroker/slurmScript.sh
+#-----------------------------------------------------
+
+# Path Name Setup:------------------------------------
 line_old='/alper/'
 line_new='/ubuntu/'
 
@@ -12,8 +24,7 @@ sed -i "s%$line_old%$line_new%g" ipfsGet.sh
 
 
 line_old='0x6af0204187a93710317542d383a1b547fa42e705'
-#line_new=$(echo $COINBASE)
-line_new='0xda1e61e853bb8d63b1426295f59cb45a34425b63'
+line_new='0xda1e61e853bb8d63b1426295f59cb45a34425b63' #line_new=$(echo $COINBASE)
 
 sed -i "s%$line_old%$line_new%g" constants.py
 sed -i "s%$line_old%$line_new%g" eBlocHeader.js
@@ -23,3 +34,4 @@ line_old='alper'
 line_new='ubuntu'
 
 sed -i "s%$line_old%$line_new%g" constants.py
+#-----------------------------------------------------
