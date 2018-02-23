9cbedd4a883a2dc3ad0d9f2ef6f922dd0e0c5364
avatar-lavventura <alper.alimoglu@gmail.com>
2018-02-23 15:23:43 +0300

    path update try.

--- ipfsStat.sh
+++ ipfsStat.sh
@@ -1,5 +1,5 @@
 #/!/bin
-IPFS_PATH="/home/whoami/.ipfs"
+IPFS_PATH=$HOME"/.ipfs"
 export IPFS_PATH
 ipfsHash=$1 
 timeout 300 ipfs object stat $ipfsHash #wait 5 minutes. TODO: ipfs call should be thread.
