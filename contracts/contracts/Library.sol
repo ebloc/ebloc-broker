e4ad55afe09a6027419027d4f8635fa67302bef1
avatar-lavventura <alper.alimoglu@gmail.com>
2018-02-25 16:16:36 +0300

    Contract Libraries merges under one, remix solidity gives error for long file names

---
+++ contracts/contracts/Library.sol
@@ -0,0 +1,180 @@
+pragma solidity ^0.4.17;
+
+library Library {
+    
+    /* Submitted Job's information */
+    struct status {
+	/* Variable assigned by the cluster */
+	uint8           status; /* Status of the submitted job {NULL, PENDING, COMPLETED, RUNNING} */
+	uint         startTime; /* Submitted job's starting universal time on the server side */
+	bool       receiptFlag; /* Prevents double spending, flag to store if receiptCheck successfully completed */
+	
+	/* Variables assigned by the client */
+	uint32   coreMinuteGas; /* Time to run job in seconds. ex: minute + hour * 60 + day * 1440; */
+	uint32           core;  /* Requested core by the client */
+	address       jobOwner; /* Address of the client (msg.sender) has been stored */
+	uint   coreMinutePrice; /* Cluster's price for core/minute */
+	uint          received; /* Paid amount by the client */
+    }
+
+    /* Registered cluster's information */
+    struct data {
+	bool               isExist;  /* Flag that checks is Cluster exists or not */
+	bool             isRunning;  /* Flag that checks is Cluster running or not */
+	uint32   memberAddressesID;  /* Cluster's ethereum address is stored */
+	uint       coreMinutePrice;  /* Should be defined in wei. Floating-point or fixed-point decimals have not yet been implemented in Solidity */
+	uint        receivedAmount;  /* Cluster's received wei price */
+	uint         blockReadFrom;  /* Blockn umber when cluster is registered in order the watch cluster's event activity */
+	bytes32             ipfsId;  /* Cluster's ipfsId */
+	string                name;  /* Cluster's name*/
+	string    clusterMiniLockId; /* Cluster's minilock-id */
+	string    federationCloudId; /* Cluster's federation cloud-id */
+
+	mapping(string => status[]) jobStatus; /* All submitted jobs into cluster 's Status is accessible */
+	intervalNode    receiptList; /* receiptList will be use to check job's start and end time overlapped or not */
+    }
+
+    struct interval {
+	uint   endpoint;
+	int32  core;
+	uint32 next; /* Points to next the node */
+    }
+
+    struct intervalNode {
+	interval[] list;
+	uint32 head;
+	uint32 coreNumber;
+	uint32 deletedItemNum;
+    }
+
+    /* Invoked, when cluster calls updateCluster */
+    function update(data storage self, string clusterName, string fID, string clusterMiniLockId, uint price, uint32 coreNumber,
+		    bytes32 ipfsId)
+	public {
+	self.name                   = clusterName;
+	self.federationCloudId      = fID;
+	self.clusterMiniLockId      = clusterMiniLockId;
+	self.coreMinutePrice        = price;
+	self.receiptList.coreNumber = coreNumber;
+	self.ipfsId                 = ipfsId;
+    }    
+
+    /* Invoked when cluster calls registerCluster() function */
+    function constructCluster(data storage self, string name, string fID, string clusterMiniLockId, uint32 memLen, uint price, uint32
+		       coreNumber, bytes32 ipfsId)
+	public {
+	self.name              = name;
+	self.federationCloudId = fID;
+	self.clusterMiniLockId = clusterMiniLockId;
+	self.ipfsId            = ipfsId;
+	self.isExist           = true;
+	self.isRunning         = true;
+	self.receivedAmount    = 0;
+	self.memberAddressesID = memLen;
+	self.coreMinutePrice   = price;
+	self.blockReadFrom     = block.number;
+
+	constructReceiptList(self.receiptList, coreNumber);
+    }
+
+    function constructReceiptList(intervalNode storage self, uint32 coreNum) 
+    {
+	self.list.push(interval({endpoint: 0, core: 0, next: 0})); /* Dummy node */
+	self.list.push(interval({endpoint: 0, core: 0, next: 0})); /* Dummy node */
+	self.head           = 1;
+	self.coreNumber     = coreNum;
+	self.deletedItemNum = 0;
+    }
+
+    function receiptCheck(intervalNode storage self, uint startTime, uint endTime, int32 coreNum) public returns (bool success)
+    {
+	bool     flag = false;
+	uint32   addr = self.head;
+	uint32   addrTemp;
+	int32    carriedSum;
+
+	interval prevNode;
+	interval currentNode;
+	interval prevNodeTemp;
+
+	// +-------------------------------+
+	// | Begin: receiptCheck Algorithm |
+	// +-------------------------------+
+
+	if (endTime < self.list[addr].endpoint) {
+	    flag         = true;
+	    prevNode     = self.list[addr];
+	    currentNode  = self.list[prevNode.next]; /* Current node points index of previous head-node right after the insert operation */
+
+	    do { /* Inside while loop carriedSum is updated */
+		carriedSum += prevNode.core;
+		if (endTime >= currentNode.endpoint) {
+		    addr = prevNode.next; /* "addr" points the index to push the node */
+		    break;
+		}
+		prevNode    = currentNode;
+		currentNode = self.list[currentNode.next];
+
+	    } while (true);
+	}
+
+	self.list.push(interval({endpoint: endTime - 1, core: coreNum, next: addr}));
+
+	if (!flag) {
+	    addrTemp      = addr;
+	    carriedSum    = coreNum;
+	    prevNode      = self.list[self.head = uint32(self.list.length-1)];
+	} else {
+	    addrTemp      = prevNode.next;
+	    prevNodeTemp  = prevNode;
+	    prevNode.next = uint32(self.list.length - 1); /* Node that pushed in-between the linked-list */
+	}
+
+	currentNode = self.list[prevNode.next]; /* Current node points index before insert operation is done */
+
+	do {
+	    if (startTime > currentNode.endpoint) { /* Covers [val, val1) s = s-1 */
+		self.list.push(interval( {endpoint: startTime, core: -1*coreNum, next: prevNode.next}));
+		prevNode.next = uint32(self.list.length - 1);
+		return true;
+	    }
+	    carriedSum += currentNode.core;
+
+	    /* If enters into if statement it means revert() is catch and all previoes operations are reverted back */
+	    if (carriedSum > int32(self.coreNumber)) {
+		delete self.list[self.list.length-1];
+		if (!flag)
+		    self.head = addrTemp;
+		else
+		    prevNodeTemp.next = addrTemp;
+
+		self.deletedItemNum += 1;
+		return false;
+	    }
+	    prevNode    = currentNode;
+	    currentNode = self.list[currentNode.next];
+	} while (true);
+
+	// +-----------------------------+
+	// | End: receiptCheck Algorithm |
+	// +-----------------------------+
+    }
+
+    /* Could be commented out, used for test */
+    function getReceiptListSize(intervalNode storage self) public view
+	returns (uint32)
+    {
+	return uint32(self.list.length-self.deletedItemNum);
+    }
+
+    /* Could be commented out, used for test */
+    function printIndex(intervalNode storage self, uint32 index) public view
+	returns (uint256, int32)
+    {
+	uint32 myIndex = self.head;
+	for (uint i = 0; i < index; i++)
+	    myIndex = self.list[myIndex].next;
+
+	return (self.list[myIndex].endpoint, self.list[myIndex].core);
+    }
+}
