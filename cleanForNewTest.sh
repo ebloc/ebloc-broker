0c6ecfa37eb845758bd6cd9965d5129bbd5234b2
avatar-lavventura <alper.alimoglu@gmail.com>
2018-03-16 14:27:11 +0300

    eBlocBrokerHeader submit() updated.

--- cleanForNewTest.sh
+++ cleanForNewTest.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo rm -rf ~/.eBlocBroker/*/*
-sudo rm ~/.eBlocBroker/my-app.pid
-sudo rm ~/.eBlocBroker/checkSinfoOut.txt
+sudo rm -f ~/.eBlocBroker/my-app.pid
+sudo rm -f ~/.eBlocBroker/checkSinfoOut.txt
 sudo rm -f nohup.out
+sudo rm -f ~/.eBlocBroker/logJobs.txt
+sudo rm -f ~/.eBlocBroker/queuedJobs.txt
