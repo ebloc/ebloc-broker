ef42a75093ae4bf7c6437d1152dc3396208d2860
avatar-lavventura <alper.alimoglu@gmail.com>
2018-03-18 15:47:05 +0300

    submitJob updated.

--- contractCalls/submitJob.py
+++ contractCalls/submitJob.py
@@ -19,22 +19,40 @@ with open('abi.json', 'r') as abi_definition:
     
 eBlocBroker = web3.eth.contract(contractAddress, abi=abi);
 
-# USER Inputs----------------------------------------------------------------
-clusterAddress = "0xc75497b304f42631d919f20db3e9b79dd59e88ff";
-name, federationCloudId, miniLockId, coreLimit, pricePerMin, ipfsID = eBlocBroker.call().getClusterInfo(clusterAddress);
-jobKey         = "3d8e2dc2-b855-1036-807f-9dbd8c6b1579=folderName";
-coreNum        = 1;
-coreGasDay     = 0;
-coreGasHour    = 0;
-coreGasMin     = 10;
-jobDescription = "Science";
-storageType    = 1;
-myMiniLockId   = "";
-# ----------------------------------------------------------------------------
-coreMinuteGas = coreGasMin + coreGasHour * 60 + coreGasDay * 1440;
-msgValue = coreNum * pricePerMin * coreMinuteGas;
-gasLimit = 3000000;
+if __name__ == '__main__': #{
+    if(len(sys.argv) == 10):
+        print("args")
+        clusterAddress = str(sys.argv[1]);
+        blockReadFrom, coreNumber, pricePerMin = eBlocBroker.call().getClusterInfo(clusterAddress);
+        jobKey         = str(sys.argv[2]);
+        coreNum        = int(sys.argv[3]);
+        coreGasDay     = int(sys.argv[4]);
+        coreGasHour    = int(sys.argv[5]);
+        coreGasMin     = int(sys.argv[6]);
+        jobDescription = str(sys.argv[7]);
+        storageType    = int(sys.argv[8]);
+        myMiniLockId   = str(sys.argv[9]);
+    else:
+        # USER Inputs----------------------------------------------------------------
+        clusterAddress = "0x6af0204187a93710317542d383a1b547fa42e705";
+        blockReadFrom, coreNumber, pricePerMin = eBlocBroker.call().getClusterInfo(clusterAddress);
+        jobKey         = "3d8e2dc2-b855-1036-807f-9dbd8c6b1579=folderName";
+        coreNum        = 1;
+        coreGasDay     = 0;
+        coreGasHour    = 0;
+        coreGasMin     = 1;
+        jobDescription = "Science";
+        storageType    = 1;
+        myMiniLockId   = "";
+        # ----------------------------------------------------------------------------        
+    coreMinuteGas = coreGasMin + coreGasHour * 60 + coreGasDay * 1440;
+    msgValue = coreNum * pricePerMin * coreMinuteGas;
+    gasLimit = 3000000;
 
-if (coreNum <= coreLimit and len(jobDescription) < 128 ):
-    tx=eBlocBroker.transact({"from": web3.eth.accounts[0], "value": msgValue, "gas": gasLimit}).submitJob(clusterAddress, jobKey, coreNum, jobDescription, coreMinuteGas, storageType, myMiniLockId);
-    print(tx);        
+    if (coreNum <= coreNumber and len(jobDescription) < 128 ):
+        tx=eBlocBroker.transact({"from": web3.eth.accounts[0], "value": msgValue, "gas": gasLimit}).submitJob(clusterAddress, jobKey, coreNum, jobDescription, coreMinuteGas, storageType, myMiniLockId);
+        print(tx);
+
+#}
+
+
