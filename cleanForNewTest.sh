58dee889a9ea74b78c7099b98bba7730d9543b96
avatar-lavventura <alper.alimoglu@gmail.com>
2018-03-22 00:18:04 +0300

    some update.

--- cleanForNewTest.sh
+++ cleanForNewTest.sh
@@ -3,8 +3,8 @@
 sudo rm -rf ~/.eBlocBroker/*/*
 sudo rm -f ~/.eBlocBroker/my-app.pid
 sudo rm -f ~/.eBlocBroker/checkSinfoOut.txt
-sudo rm -f nohup.out
 sudo rm -f ~/.eBlocBroker/logJobs.txt
 sudo rm -f ~/.eBlocBroker/queuedJobs.txt
+sudo cat /dev/null > nohup.out
 
-sudo bash killall.sh
+# sudo bash killall.sh
--- log.js
+++ log.js
@@ -1,8 +1,10 @@
 var nodePaths = require('./nodePaths');
 var eBlocBroker     = require(nodePaths.EBLOCBROKER + '/eBlocBrokerHeader.js');
 
-readFrom = 1804638; //eBlocBroker.getDeployedBlockNumber();
-eBlocBroker.LogJob(readFrom, nodePaths.LOG_PATH + '/queuedJobs.txt' ); /* All jobs sent */
+readFrom = 1805917; //eBlocBroker.getDeployedBlockNumber();
+clusterID = "0xe056d08f050503c1f068dc81fc7f7b705fc2c503";
+
+eBlocBroker.LogJob(readFrom, nodePaths.LOG_PATH + '/queuedJobs.txt', clusterID); /* All jobs sent */
 
 //eBlocBroker.LogReceipt(readFrom, nodePaths.LOG_PATH + '/queuedJobs.txt');
 
