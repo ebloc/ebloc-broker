6002617af19bdcb870acedc4d666cb0edf1a850b
avatar-lavventura <alper.alimoglu@gmail.com>
2018-04-02 19:06:44 +0300

    more update.

--- nodePaths.js
+++ nodePaths.js
@@ -1,5 +1,6 @@
 module.exports = Object.freeze({
-    EBLOCBROKER: 'EBLOCBROKER_PATH',
-    LOG_PATH:    '/home/whoami/.eBlocBroker'
+    EBLOCBROKER: '/home/alper/eBlocBroker',
+    LOG_PATH:    '/home/alper/.eBlocBroker',
+    CLUSTER_ID:   '0xffffffffffffffffffffffffffffffffffffffff' //Should be the address of the cluster.
 });
 
