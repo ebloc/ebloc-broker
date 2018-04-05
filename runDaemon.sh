b38d6b485dc37f0fafb04398c0fd550054995ef4
avatar-lavventura <alper.alimoglu@gmail.com>
2018-04-05 10:58:38 +0300

    more update.

--- runDaemon.sh
+++ runDaemon.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+source $HOME/.venv-py3/bin/activate
+
 if [[ $EUID -ne 0 ]]; then
-    nohup python -u Driver.py &
-    sudo tail -f  nohup.out
+    nohup python -u Driver.py > clusterDriver.out 2>&1 &
+    sudo tail -f  clusterDriver.out
 else
     echo "This script must be run as non-root. Please run without 'sudo'." 
 fi
