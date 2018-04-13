2ac901a396fef5d45d9601e4f53d6fcc029a23cb
avatar-lavventura <alper.alimoglu@gmail.com>
2018-04-13 19:03:59 +0300

    more update.

--- slurmScript.sh
+++ slurmScript.sh
@@ -3,36 +3,44 @@
 a=$(echo $0)
 b=$(echo $1)
 c=$(echo $2)
-event=$(echo $2 | awk 'print $4')
+event=$(echo $c | awk '{print $8}')
+echo "Your message | $a | $b | $c //$event ." | mail -s "Message Subject" alper.alimoglu@gmail.com
 
-scriptPath="EBLOCBROKER_PATH"; 
+scriptPath="/home/alper/eBlocBroker"; 
 
-echo "Your message | $a | $b | $c | $event  " | mail -s "Message Subject" alper.alimoglu@gmail.com
-
-if [[ $c == *"Began"* ]]; then
+if [[ $c == *" Began, "* ]]; then
     name=$(echo "$c"  | grep -o -P '(?<=Name=).*(?=.sh Began)')
     arg0=$(echo $name | cut -d "_" -f 1)
     arg1=$(echo $name | cut -d "_" -f 2)
 
-    python $scriptPath/startCode.py $arg0 $arg1 
     echo "JOB STARTED: $name |$arg0 $arg1 " | mail -s "Message Subject" alper.alimoglu@gmail.com
+    python $scriptPath/startCode.py $arg0 $arg1     
 fi
 
-if [[ $c == *"COMPLETED"* ]]; then #cancelled job won't catched here.
+if [[ $event == *"COMPLETED"* ]]; then # Completed slurm jobs are catched here
     name=$(echo "$c"   | grep -o -P '(?<=Name=).*(?=.sh Ended)')
     argu0=$(echo $name | cut -d "_" -f 1)
     argu1=$(echo $name | cut -d "_" -f 2)
     argu2=$(echo $name | cut -d "_" -f 3) 
     argu3=$(echo $name | cut -d "_" -f 4) 
-    argu4=$(echo $name | cut -d "_" -f 5) 
-    python $scriptPath/endCode.py $argu0 $argu1 $argu2 $argu3 $argu4 $name
+    argu4=$(echo $name | cut -d "_" -f 5)
+    
     echo "COMPLETED fileName:$name |argu0:$argu0 argu1:$argu1 argu2:$argu2 argu3:$argu3 argu4:$argu4" | mail -s "Message Subject" alper.alimoglu@gmail.com
+    python $scriptPath/endCode.py $argu0 $argu1 $argu2 $argu3 $argu4 $name    
 fi
 
-if [[ $event == *" Failed, "* ]]; then #cancelled job won't catched here.
-
+if [[ $event == *"TIMEOUT"* ]]; then # Timeouted slurm jobs are catched here
+    name=$(echo "$c"   | grep -o -P '(?<=Name=).*(?=.sh Failed)')
+    argu0=$(echo $name | cut -d "_" -f 1)
+    argu1=$(echo $name | cut -d "_" -f 2)
+    argu2=$(echo $name | cut -d "_" -f 3) 
+    argu3=$(echo $name | cut -d "_" -f 4) 
+    argu4=$(echo $name | cut -d "_" -f 5)
+    
+    echo "TIMEOUT fileName:$name |argu0:$argu0 argu1:$argu1 argu2:$argu2 argu3:$argu3 argu4:$argu4" | mail -s "Message Subject" alper.alimoglu@gmail.com
+    python $scriptPath/endCode.py $argu0 $argu1 $argu2 $argu3 $argu4 $name    
 fi
 
-if [[ $event == *"TIMEOUT"* ]]; then #cancelled job won't catched here.
+if [[ $event == *" Failed, "* ]]; then # Cancelled job won't catched here
 
 fi
