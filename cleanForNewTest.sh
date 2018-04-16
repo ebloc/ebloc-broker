09da5f475952a60315ea8cb5dd2bd431201903f7
avatar-lavventura <alper.alimoglu@gmail.com>
2018-04-16 14:06:36 +0300

    more update.

--- cleanForNewTest.sh
+++ cleanForNewTest.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo rm -rf $HOME/.eBlocBroker/*/*
-sudo rm -f  $HOME/.eBlocBroker/my-app.pid
-sudo rm -f  $HOME/.eBlocBroker/checkSinfoOut.txt
-sudo rm -f  $HOME/.eBlocBroker/logJobs.txt
-sudo rm -f  $HOME/.eBlocBroker/queuedJobs.txt
-sudo rm -f  $HOME/.eBlocBroker/test.txt
+rm -rf $HOME/.eBlocBroker/*/*
+rm -f  $HOME/.eBlocBroker/my-app.pid
+rm -f  $HOME/.eBlocBroker/checkSinfoOut.txt
+rm -f  $HOME/.eBlocBroker/logJobs.txt
+rm -f  $HOME/.eBlocBroker/queuedJobs.txt
+rm -f  $HOME/.eBlocBroker/test.txt
 
-sudo cat /dev/null > clusterDriver.out
+cat /dev/null > clusterDriver.out
 
 sudo bash killall.sh
 sudo bash clean.sh
