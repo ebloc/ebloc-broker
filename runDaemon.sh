675fd8ed3a839fbe6bc891885d4fa56212f5db37
avatar-lavventura <alper.alimoglu@gmail.com>
2018-04-24 18:21:44 +0300

    some update.

--- runDaemon.sh
+++ runDaemon.sh
@@ -3,8 +3,8 @@
 # source $HOME/.venv-py3/bin/activate
 . venv/bin/activate
 
-gopath=$(go env | grep 'GOPATH' | cut -d "=" -f 2 | tr -d '"');
-export PATH=$PATH:$gopath/bin;
+# gopath=$(go env | grep 'GOPATH' | cut -d "=" -f 2 | tr -d '"');
+# export PATH=$PATH:$gopath/bin;
 
 if [[ $EUID -ne 0 ]]; then
     nohup python -Bu Driver.py > clusterDriver.out 2>&1 &
--- runSlurm.sh
+++ runSlurm.sh
@@ -2,13 +2,18 @@
 # To Run:  sudo bash runSlurm.sh
 #------------------------------
 
-sudo killall slurmctld slurmdbd slurmd
+if [[ $EUID -e 0 ]]; then
+    sudo killall slurmctld slurmdbd slurmd
+    sudo munged -f
+    sudo /etc/init.d/munge start 
+    sudo slurmd
+    slurmdbd &
+    sudo -u $(logname) mkdir -p /tmp/slurmstate
+    sudo chown -R $(logname) /tmp/slurmstate
+    slurmctld -c #-cDvvvvvv
+    sinfo
+else
+    echo "This script must be run as root. Please run:  sudo bash runSlurm.sh" 
+fi
 
-sudo munged -f
-sudo /etc/init.d/munge start 
-sudo slurmd
-slurmdbd &
-sudo -u $(logname) mkdir -p /tmp/slurmstate
-sudo chown -R $(logname) /tmp/slurmstate
-slurmctld -c #-cDvvvvvv
-sinfo
+
