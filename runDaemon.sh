0c4f452c54e23b754d694185a0bf948de570b88e
avatar-lavventura <alper.alimoglu@gmail.com>
2018-04-24 10:00:50 +0300

    some update.

--- runDaemon.sh
+++ runDaemon.sh
@@ -7,7 +7,7 @@ gopath=$(go env | grep 'GOPATH' | cut -d "=" -f 2 | tr -d '"');
 export PATH=$PATH:$gopath/bin;
 
 if [[ $EUID -ne 0 ]]; then
-    nohup python -u Driver.py > clusterDriver.out 2>&1 &
+    nohup python -Bu Driver.py > clusterDriver.out 2>&1 &
     sudo tail -f  clusterDriver.out
 else
     echo "This script must be run as non-root. Please run without 'sudo'." 
