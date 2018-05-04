c1c4471a418fc4c57efc9a3ca10ac0a7cdf86c45
avatar-lavventura <alper.alimoglu@gmail.com>
2018-05-04 10:38:34 +0300

    update.

--- generateTest/newTest.sh
+++ generateTest/newTest.sh
@@ -1,17 +1,19 @@
+#!/bin/bash
+
 killall python
 
-cd /home/prc/multiple/workingTestIpfs
-bash /home/prc/multiple/workingTestIpfs/cleanForNewTest.sh
-bash /home/prc/multiple/workingTestIpfs/runDaemon.sh a
+cd workingTestIpfs
+bash cleanForNewTest.sh
+bash runDaemon.sh a
 
-cd /home/prc/multiple/workingTestIpfsMiniLock
-bash /home/prc/multiple/workingTestIpfsMiniLock/cleanForNewTest.sh
-bash /home/prc/multiple/workingTestIpfsMiniLock/runDaemon.sh a
+cd ../workingTestIpfsMiniLock
+bash cleanForNewTest.sh
+bash runDaemon.sh a
 
-cd /home/prc/multiple/nasEudat
-bash /home/prc/multiple/nasEudat/cleanForNewTest.sh
-bash /home/prc/multiple/nasEudat/runDaemon.sh a
+cd ../nasEudat
+bash cleanForNewTest.sh
+bash runDaemon.sh a
 
-cd /home/prc/multiple/workingGdrive
-bash /home/prc/multiple/workingGdrive/cleanForNewTest.sh
-bash /home/prc/multiple/workingGdrive/runDaemon.sh a
+cd ../workingGdrive
+bash cleanForNewTest.sh
+bash runDaemon.sh a
