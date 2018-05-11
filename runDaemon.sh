f0c8519da2a6c3785ff78435e6d101e2f4752c6d
avatar-lavventura <alper.alimoglu@gmail.com>
2018-05-11 12:19:01 +0300

    Update.

--- runDaemon.sh
+++ runDaemon.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/bash                      |
+#                                |
+# To Run:  sudo bash runDaemon.sh|
+#---------------------------------
 
-# source $HOME/.venv-py3/bin/activate
-. venv/bin/activate
-
-# gopath=$(go env | grep 'GOPATH' | cut -d "=" -f 2 | tr -d '"');
-# export PATH=$PATH:$gopath/bin;
+. venv/bin/activate # source $HOME/.venv-py3/bin/activate
 
 if [[ "$EUID" -ne 0 ]]; then
     nohup python -Bu Driver.py > $HOME/.eBlocBroker/clusterDriver.out 2>&1 &
@@ -13,3 +12,5 @@ else
     echo "This script must be run as non-root. Please run without 'sudo'." 
 fi
 
+# gopath=$(go env | grep 'GOPATH' | cut -d "=" -f 2 | tr -d '"');
+# export PATH=$PATH:$gopath/bin;
--- tar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for a in $1/*.tar.gz; do
-    if [[ "$a" != $1/result-* ]] ; then
-	tar -xvf "$a" -C $1;
-    fi
-done;
