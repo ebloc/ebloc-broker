029b555143a868fc67823074ab5b860b59d2fe8e
avatar-lavventura <alper.alimoglu@gmail.com>
2018-05-16 00:39:33 +0300

    Update orcID and github user name added on user registreation.

--- contractCalls/registerUser.py
+++ contractCalls/registerUser.py
@@ -29,18 +29,21 @@ if __name__ == '__main__': #{
     if len(sys.argv) == 6:
         account = web3.eth.accounts[int(sys.argv[1])];
         userEmail          = str(sys.argv[2]);
-        federationCloudId  = str(sys.argv[3]);
+        federationCloudID  = str(sys.argv[3]);
         miniLockID         = str(sys.argv[4]);
         ipfsAddress        = str(sys.argv[5]);
     else:
         account            = web3.eth.accounts[0]; # User's Ethereum Address
         userEmail          = "alper.alimoglu@gmail.com";
-        federationCloudId  = "3d8e2dc2-b855-1036-807f-9dbd8c6b1579";
+        federationCloudID  = "3d8e2dc2-b855-1036-807f-9dbd8c6b1579";
         miniLockID         = "9VZyJy1gRFJfdDtAjRitqmjSxPjSAjBR6BxH59UeNgKzQ";
-        ipfsAddress        = "/ip4/79.123.177.145/tcp/4001/ipfs/QmWmZQnb8xh3gHf9ZFmVQC4mLEav3Uht5kHJxZtixG3rsf";
+        ipfsAddress        = "/ip4/79.123.177.145/tcp/4001/ipfs/QmWmZQnb8xh3gHf9ZFmVQC4mLEav3Uht5kHJxZtixG3rsf";        
+        githubUserName     = "eBloc";
+        orcID              = "0000-0001-7642-0552";
     # ----------------------------------------------------------------------------
-
-    if len(federationCloudId) < 128 and len(userEmail) < 128: #{
-        tx = eBlocBroker.transact({"from":account, "gas": 4500000}).registerUser(userEmail, federationCloudId, miniLockID, ipfsAddress);
+    
+    if len(federationCloudID) < 128 and len(userEmail) < 128: #{
+        tx = eBlocBroker.transact({"from":account, "gas": 4500000}).registerUser(userEmail, federationCloudID, miniLockID, ipfsAddress);
+        # tx = eBlocBroker.transact({"from":account, "gas": 4500000}).registerUser(userEmail, federationCloudID, miniLockID, ipfsAddress, githubUserName, orcID);
         print('Tx: ' + tx.hex());
 #}
