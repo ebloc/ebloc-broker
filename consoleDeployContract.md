8ecca5673103ab1448edf5f024fe062ce764c815
avatar-lavventura <alper.alimoglu@gmail.com>
2018-05-19 14:39:28 +0300

    update.

--- consoleDeployContract.md
+++ consoleDeployContract.md
@@ -21,6 +21,21 @@ var linkedListLib = myLinkedListLib.new({ from: eth.accounts[0], data: "0x" + te
     console.log(e, contract);
     if (typeof contract.address !== 'undefined') {
          console.log('Lib mined! address: ' + contract.address.substring(2) + ' transactionHash: ' + contract.transactionHash);
+
+
+         var arrayCode = testOutput.contracts["e.sol:eBlocBroker"].bin.replace(/__e.sol:Lib__________________+/g, contract.address.substring(2) )
+         var myArray   = web3.eth.contract(JSON.parse(testOutput.contracts["e.sol:eBlocBroker"].abi));
+
+         var eBlocBroker = myArray.new({ from: eth.accounts[0], data: "0x" + arrayCode, gas: 4700000},
+           function (e, contract) {
+              console.log(e, contract);
+              if (typeof contract.address !== 'undefined') {
+                  console.log('eBlocBroker mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
+              }
+           }
+        );
+
+		 
     }
   }
 );
--- contractCalls/getUserInfo.py
+++ contractCalls/getUserInfo.py
@@ -27,29 +27,29 @@ if __name__ == '__main__': #{
 
     userAddress = web3.toChecksumAddress(userAddress);
     
-    if (str(eBlocBroker.functions.isUserExist(userAddress).call()) == "False"):
+    if (str(eBlocBroker.functions.isUserExist(userAddress).call()) == "False"): #{
         print("User is not registered. Please try again with registered Ethereum Address as user. \nYou can register your user using: registerUser.py script.")
         sys.exit();
-
-    blockReadFrom, orcid , orcidVerify = eBlocBroker.functions.getUserInfo(userAddress).call();
-
-    my_filter = eBlocBroker.eventFilter('LogUser',{'fromBlock':int(blockReadFrom),'toBlock':int(blockReadFrom) + 1})
-    # my_filter = eBlocBroker.eventFilter('LogUser',{'fromBlock':int(blockReadFrom + 1),'toBlock':int(blockReadFrom)})
-
+    #}
     
-    if printType == '0':
+    blockReadFrom, orcid = eBlocBroker.functions.getUserInfo(userAddress).call();       
+    my_filter = eBlocBroker.eventFilter('LogUser',{'fromBlock':int(blockReadFrom),'toBlock':int(blockReadFrom) + 1})
+    
+    if printType == '0': #{
         print('{0: <17}'.format('blockReadFrom: ') + str(blockReadFrom))    
         print('{0: <17}'.format('userEmail: ')     + my_filter.get_all_entries()[0].args['userEmail'])
         print('{0: <17}'.format('miniLockID: ')    + my_filter.get_all_entries()[0].args['miniLockID'])
         print('{0: <17}'.format('ipfsAddress: ')   + my_filter.get_all_entries()[0].args['ipfsAddress'])   
         print('{0: <17}'.format('fID: ')           + my_filter.get_all_entries()[0].args['fID'])
-        print('{0: <17}'.format('orcid: ')         + orcid)
-        print('{0: <17}'.format('orcidVerify: ')   + str(orcidVerify))
-    else:
+        print('{0: <17}'.format('orcid: ')         + orcid)        
+        print('{0: <17}'.format('orcidVerify: ')   + str(eBlocBroker.functions.isOrcIdVerified(orcid).call()))
+    #}
+    else: #{
         print(str(blockReadFrom) + ',' +
               my_filter.get_all_entries()[0].args['userEmail']   + ',' +
               my_filter.get_all_entries()[0].args['miniLockID']  + ',' +
               my_filter.get_all_entries()[0].args['ipfsAddress'] + ',' +
               my_filter.get_all_entries()[0].args['fID']         + ',' 
         );
+    #}
 #}
