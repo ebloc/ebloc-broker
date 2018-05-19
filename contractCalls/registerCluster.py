5fed2e10fad2e4d5b49f11e67eb3795426260a7d
avatar-lavventura <alper.alimoglu@gmail.com>
2018-05-19 16:18:23 +0300

    update.

--- contractCalls/registerCluster.py
+++ contractCalls/registerCluster.py
@@ -32,11 +32,12 @@ miniLockId         = "9VZyJy1gRFJfdDtAjRitqmjSxPjSAjBR6BxH59UeNgKzQ";
 corePriceMinuteWei = 100;
 ipfsAddress        = "/ip4/79.123.177.145/tcp/4001/ipfs/QmWmZQnb8xh3gHf9ZFmVQC4mLEav3Uht5kHJxZtixG3rsf";
 # ----------------------------------------------------------------------------
-#os.environ['ipfs'] = ipfs;
-#ipfsID=os.popen('node bs58.js encode $ipfs').read().replace("\n", "");
-#ipfsIDbytes = web3.toBytes(hexstr=ipfsID);
 
 if len(federationCloudId) < 128 and len(clusterEmail) < 128 and (len(miniLockId) == 0 or len(miniLockId) == 45): #{
     tx = eBlocBroker.transact({"from":account, "gas": 4500000}).registerCluster(coreNumber, clusterEmail, federationCloudId, miniLockId, corePriceMinuteWei, ipfsAddress);
     print('Tx: ' + tx.hex());
 #}
+
+#os.environ['ipfs'] = ipfs;
+#ipfsID=os.popen('node bs58.js encode $ipfs').read().replace("\n", "");
+#ipfsIDbytes = web3.toBytes(hexstr=ipfsID);
