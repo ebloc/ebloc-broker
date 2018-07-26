9939a5f356b81b60d8a215e47d4c01e2813ea3b6
avatar-lavventura <alper.alimoglu@gmail.com>
2018-07-26 22:03:47 +0300

    LogJon api is converted from web3.js into web3.py.

--- contractCalls/authenticateORCID.py
+++ contractCalls/authenticateORCID.py
@@ -3,7 +3,6 @@
 from imports import *
 
 if __name__ == '__main__': #{
-    account            = web3.eth.accounts[0];
     # USER Inputs----------------------------------------------------------------
     if len(sys.argv) == 2:
         orcID = str(sys.argv[1]);
@@ -11,7 +10,7 @@ if __name__ == '__main__': #{
         orcID = "0000-0001-7642-0552";
     # ----------------------------------------------------------------------------
 
-    
+    account = web3.eth.accounts[0];    
     if eBlocBroker.functions.isOrcIdVerified(orcID).call() == 0:
         tx = eBlocBroker.transact({"from":account, "gas": 4500000}).authenticateORCID(orcID);
         print('Tx: ' + tx.hex());
