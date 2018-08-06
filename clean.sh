8b4a65b3368718ab4f6d1a76e5e9eb3f42cb5478
avatar-lavventura <alper.alimoglu@gmail.com>
2018-08-06 17:41:06 +0300

    update.

--- clean.sh
+++ clean.sh
@@ -5,24 +5,22 @@
 
 pyclean docs/
 
-rm -rf docs/_build_html
-rm -rf docs/__pycache__
-rm -rf contract/tests/__pycache__
-rm -rf __pycache__/
-rm -rf docs/_build
-rm -rf contractCalls/__pycache__/
+find . -name '*.pyc' -delete
+find . -name 'nohup.out' -delete
+
 rm -f  contractCalls/*.*~
-rm -f  contractCalls/nohup.out
 rm -f checkSinfoOut.txt
 rm -f .node-xmlhttprequest*
 rm -f *.*~
 rm -f docs/*.*~
-rm -f docs/nohup.out
-rm -f nohup.out
 rm -f ./docs/solidity_lexer.pyc
 rm -f ipfs.out
 rm -f modifiedDate.txt
 rm -f \#*
-find . -name '*.pyc' -delete
 
-
+rm -rf contract/tests/__pycache__/
+rm -rf contractCalls/__pycache__/
+rm -rf docs/_build_html/
+rm -rf docs/__pycache__/
+rm -rf __pycache__/
+rm -rf docs/_build/
