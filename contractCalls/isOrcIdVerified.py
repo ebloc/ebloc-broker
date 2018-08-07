5bc59a9e4b0f0bd1bdd149a68414d0b75d3be7d0
avatar-lavventura <alper.alimoglu@gmail.com>
2018-08-07 22:44:09 +0300

    slurmScript is updated, driverFunc bugs are fixed.

--- contractCalls/isOrcIdVerified.py
+++ contractCalls/isOrcIdVerified.py
@@ -4,9 +4,12 @@ from imports import *
 
 if __name__ == '__main__': #{
     if len(sys.argv) == 2:
-        orcid = str(sys.argv[1]);
+        orcID = str(sys.argv[1]);
     else:
-        orcid = "0000-0001-7642-0552";
-
-    print(eBlocBroker.functions.isOrcIdVerified(orcid).call())
+        orcID = "0000-0001-7642-0552";
+        
+    if eBlocBroker.functions.isOrcIdVerified(orcID).call() == 0:
+        print('False');
+    else:
+        print('True');
 #}
