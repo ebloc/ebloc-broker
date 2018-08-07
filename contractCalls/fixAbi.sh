c0f13f256bc0859d46233a581b1b7351e9e912cb
avatar-lavventura <alper.alimoglu@gmail.com>
2018-08-07 14:32:26 +0300

    update.

--- contractCalls/fixAbi.sh
+++ contractCalls/fixAbi.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+firstChar=$(head -c 1 abi.json)
+if  [ "$firstChar" = "[" ]; then
+    echo "Already fixed."
+    exit;                      
+fi
+
+
 sed -i 's/\"\[/\[/g' abi.json
 sed -i 's/\]\"/\]/g' abi.json
 sed -i 's/\\\"/\"/g' abi.json
