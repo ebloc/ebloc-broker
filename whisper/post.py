ad1e2ee19f9f77a7d98000708508ccf9dc4d478f
avatar-lavventura <alper.alimoglu@gmail.com>
2018-09-07 22:10:23 +0300

    whisper more update.

--- whisper/post.py
+++ whisper/post.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from web3 import Web3, HTTPProvider
 web3 = Web3(HTTPProvider('http://localhost:8545'))
 from web3.shh import Shh
--- whisper/receiverFilter.py
+++ whisper/receiverFilter.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from web3 import Web3, HTTPProvider
 web3 = Web3(HTTPProvider('http://localhost:8545'))
 from web3.shh import Shh
@@ -9,27 +11,25 @@ from hexbytes import (
     HexBytes,
 )
 
-kId = web3.shh.newKeyPair()
+loadFlag = 1
+
+if loadFlag == 1:
+    privateKey = '0x5fc212a0774add56f4a32b59a1cf6100ae0ef8fe1481c1ab7d011796d1e53320' 
+    kId = web3.shh.addPrivateKey(privateKey)
+else:
+    kId          = web3.shh.newKeyPair()
+
 receiver_pub = web3.shh.getPublicKey(kId)
-
-privateKey = '0x5fc212a0774add56f4a32b59a1cf6100ae0ef8fe1481c1ab7d011796d1e53320'
-kId = web3.shh.addPrivateKey(privateKey)
-
 print('receiverPubK: ' + receiver_pub);
-print('receiverPrivateK: ' + web3.shh.getPrivateKey(kId));
 
-topic = '0x13370000'
-
-shh_filter = web3.shh.newMessageFilter({
-    'privateKeyID': kId,
-    'topics': [topic]
-})
+myFilter = web3.shh.newMessageFilter({'topic': '0x07678231', 'privateKeyID': kId})
+print('FilterID: ' + myFilter.filter_id)
 
 input("Press Enter to continue...");
 
 received_messages = [];
-received_messages = shh_filter.get_new_entries()
-print(len(received_messages)) # Returns '0'
+received_messages = myFilter.get_new_entries()
+print(len(received_messages)) 
 
 print(received_messages[0])
-print(web3.shh.info.memory)  # Returns '0'
+print(web3.shh.info.memory)  
