48a2c7a89874c151f2485e672835743d4c440e74
avatar-lavventura <alper.alimoglu@gmail.com>
2018-09-17 18:55:17 +0300

    LogJob updated.

--- contractCalls/LogJob.py
+++ contractCalls/LogJob.py
@@ -6,15 +6,16 @@ asynchronous polling: http://web3py.readthedocs.io/en/latest/filters.html#exampl
 
 import sys, asyncio, time
 from web3.auto import w3
-   
+
+# -----------------
 def handle_event(event):
-    '''
-    print(event.blockNumber)
-    print(event.args.clusterAddress)
-    print(event.args.jobKey)
-    print(event.args.index)
-    print(event.args.storageID)
-    '''
+
+    # print(event.blockNumber)
+    # print(event.args.clusterAddress)
+    # print(event.args.jobKey)
+    # print(event.args.index)
+    # print(event.args.storageID)
+
     print(event)
 
 def log_loop(event_filter, poll_interval): #{
@@ -33,50 +34,15 @@ def log_loop(event_filter, poll_interval): #{
     #}
 #}    
 
-def log_ret(event_filter, poll_interval): #{    
-    while True: #{
-        loggedJobs = event_filter.get_new_entries() 
-        if len(loggedJobs) > 0:
-            return loggedJobs         
-        time.sleep(poll_interval)              
-    #}
-#}    
-
-def runLogJob(fromBlock, clusterAddress, eBlocBroker): #{
-   myFilter = eBlocBroker.events.LogJob.createFilter(
-       fromBlock=int(fromBlock),       
-       argument_filters={'clusterAddress': str(clusterAddress)}
-   )    
-   loggedJobs = myFilter.get_all_entries() 
-
-   if len(loggedJobs) > 0:       
-       return loggedJobs 
-   else:
-       return log_ret(myFilter, 2) 
-#}
-
-def runLogCancelRefund(fromBlock, clusterAddress, eBlocBroker): #{
-   myFilter = eBlocBroker.events.LogCancelRefund.createFilter(
-       fromBlock=int(fromBlock),       
-       # argument_filters={'clusterAddress': str(clusterAddress)}
-   )
-   loggedJobs = myFilter.get_all_entries() 
-
-   if len(loggedJobs) > 0:       
-       return loggedJobs 
-   else:
-       return log_ret(myFilter, 2) 
-#}
-
 def logJob(eBlocBroker=None): #{
    if eBlocBroker is None: #{
        import os 
        sys.path.insert(1, os.path.join(sys.path[0], '..')) 
        from imports import connectEblocBroker
-       eBlocBroker    = connectEblocBroker()
+       eBlocBroker = connectEblocBroker()
    #}
    
-   loggedJobs = run(fromBlock, clusterAddress, eBlocBroker) 
+   loggedJobs = runLogJob(fromBlock, clusterAddress, eBlocBroker) 
 
    # print(myFilter.filter_params)
    if len(loggedJobs) == 0:
@@ -93,14 +59,77 @@ def logJob(eBlocBroker=None): #{
            print(loggedJobs[i].args['desc']) 
    #}    
 #}
+# -----------------
+
+def getEbloBroker(): #{
+    import os 
+    sys.path.insert(1, os.path.join(sys.path[0], '..')) 
+    from imports import connectEblocBroker
+    return connectEblocBroker()
+#}
+
+def logReturn(event_filter, poll_interval): #{    
+    while True: #{
+        loggedJobs = event_filter.get_new_entries() 
+        if len(loggedJobs) > 0:
+            return loggedJobs         
+        time.sleep(poll_interval)              
+    #}
+#}    
+
+def runLogJob(fromBlock, clusterAddress, eBlocBroker=None): #{
+   if eBlocBroker is None: #{
+       eBlocBroker = getEbloBroker()
+   #}
+
+   myFilter = eBlocBroker.events.LogJob.createFilter(
+       fromBlock=int(fromBlock),       
+       argument_filters={'clusterAddress': str(clusterAddress)}
+   )    
+   loggedJobs = myFilter.get_all_entries() 
+
+   if len(loggedJobs) > 0:       
+       return loggedJobs 
+   else:
+       return logReturn(myFilter, 2) 
+#}
+
+def runLogCancelRefund(fromBlock, clusterAddress, eBlocBroker=None): #{
+   if eBlocBroker is None: #{
+       eBlocBroker = getEbloBroker()
+   #}
+   myFilter = eBlocBroker.events.LogCancelRefund.createFilter(
+       fromBlock=int(fromBlock),       
+       # argument_filters={'clusterAddress': str(clusterAddress)} #TODO: uncomment
+   )
+   loggedJobs = myFilter.get_all_entries() 
+
+   if len(loggedJobs) > 0:       
+       return loggedJobs 
+   else:
+       return logReturn(myFilter, 2) 
+#}
 
 if __name__ == '__main__': #{
-   if len(sys.argv) == 2:
+   if len(sys.argv) == 2: #{
         fromBlock      = int(sys.argv[1]) 
         clusterAddress = str(sys.argv[2]) # Only obtains jobs that are submitted to the cluster.
-        logJob() 
-   else:
+   #}
+   else: #{
        fromBlock      = 954795 
-       clusterAddress = '0x4e4a0750350796164d8defc442a712b7557bf282' 
-       logJob()    
+       clusterAddress = '0x4e4a0750350796164d8defc442a712b7557bf282'
+   #}   
+
+   loggedJobs = runLogJob(fromBlock, clusterAddress)
+   for i in range(0, len(loggedJobs)):
+           print(loggedJobs[i])
+           print(loggedJobs[i]['blockNumber'])
+           print(loggedJobs[i].args['clusterAddress'])
+           print(loggedJobs[i].args['jobKey'])
+           print(loggedJobs[i].args['index'])
+           print(loggedJobs[i].args['storageID'])
+           print(loggedJobs[i].args['desc'])
+   #}   
 #}
+
+
