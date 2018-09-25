43840c997523c85df38cc100ad0d92dd9f6285ee
avatar-lavventura <alper.alimoglu@gmail.com>
2018-09-25 15:58:04 +0300

    update.

--- rsync.sh
+++ rsync.sh
@@ -8,5 +8,5 @@ toFolder='/Users/alper/eBlocBroker';
 if [ -z "$(ls -A $sourceFolder)" ]; then 
     echo "Please do: sshfs alper@ebloc.cmpe.boun.edu.tr:/home/alper/eBlocBroker $sourceFolder" 
 else 
-    rsync -av --progress $sourceFolder/ $toFolder --exclude venv --exclude 'nohup.out' --exclude node_modules --exclude='.git' --exclude nodePaths.js --exclude /README.md --exclude contract --exclude .gitignore --filter="dir-merge,- .gitignore" --delete 
+    rsync -av --progress $sourceFolder/ $toFolder --exclude venv --exclude 'nohup.out' --exclude node_modules --exclude='.git' --exclude .env --exclude /README.md --exclude contract --exclude .gitignore --filter="dir-merge,- .gitignore" --delete 
 fi 
