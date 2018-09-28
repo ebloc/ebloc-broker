bf272171b6fdc888a0a501dad3a48f9e04edaaf7
avatar-lavventura <alper.alimoglu@gmail.com>
2018-09-28 20:48:27 +0300

    EUDAT cache is added.

--- owncloudScripts/README.md
+++ owncloudScripts/README.md
@@ -1,3 +1,5 @@
 # Mount
 
+```
 sudo mount.davfs https://b2drop.eudat.eu/remote.php/webdav/ oc
+```
--- removeUser.sh
+++ removeUser.sh
@@ -4,3 +4,4 @@ account=$1
 sudo userdel $account
 sacctmgr remove user where user=$account --immediate
 sudo rm -rf /var/eBlocBroker/$1
+echo 'Done'
