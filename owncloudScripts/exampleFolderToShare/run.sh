4457e82ba934b48ea2510a87b63d906f15404dca
avatar-lavventura <alper.alimoglu@gmail.com>
2018-09-28 11:24:22 +0300

    update

---
+++ owncloudScripts/exampleFolderToShare/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -o slurm.out        # STDOUT
+#SBATCH -e slurm.err        # STDERR
+#SBATCH --mail-type=ALL
+
+g++ helloworld.cpp -o hello
+./hello
+sleep 300
+
+echo completed 300 > completed.txt
