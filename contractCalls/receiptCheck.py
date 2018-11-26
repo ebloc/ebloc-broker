231707602170cd5557ebc8a1ee6cc5a711c5690b
avatar-lavventura <alper.alimoglu@gmail.com>
2018-11-26 19:45:16 +0300

    update

--- contractCalls/receiptCheck.py
+++ contractCalls/receiptCheck.py
@@ -4,28 +4,29 @@ import sys, os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 import lib
 
-def receiptCheck(jobKey, index, jobRunTimeMinute, resultIpfsHash, storageID, endTime, eBlocBroker=None, web3=None): #{
+def receiptCheck(jobKey, index, jobRunTimeMinute, resultIpfsHash, storageID, endTime, usedBandwidthMB, eBlocBroker=None, web3=None): 
     if (storageID == 0 and len(resultIpfsHash) != 46) or (storageID == 2 and len(resultIpfsHash) != 46):
         return "jobKey's length does not match with its original length. Please check your jobKey."
     
-    if eBlocBroker == None and web3 == None: #{
+    if eBlocBroker == None and web3 == None:
         from imports import connectEblocBroker
         from imports import getWeb3
         web3        = getWeb3()
-        eBlocBroker = connectEblocBroker(web3)
-    #}
-    tx = eBlocBroker.transact({"from":web3.toChecksumAddress(lib.CLUSTER_ID), "gas": 4500000}).receiptCheck(str(jobKey), int(index), int(jobRunTimeMinute), str(resultIpfsHash), int(storageID), int(endTime)) 
+        eBlocBroker = connectEblocBroker(web3)    
+    tx = eBlocBroker.transact({"from":web3.toChecksumAddress(lib.CLUSTER_ID),
+                               "gas": 4500000}).receiptCheck(str(jobKey), int(index), int(jobRunTimeMinute),
+                                                             str(resultIpfsHash), int(storageID), int(endTime), int(usedBandwidthMB)) 
     return 'Tx: ' + tx.hex()
-#}
-if __name__ == '__main__': #{
-    if len(sys.argv) == 7: #{
+
+if __name__ == '__main__': 
+    if len(sys.argv) == 8:
         jobKey           = str(sys.argv[1]) 
         index            = int(sys.argv[2]) 
         jobRunTimeMinute = int(sys.argv[3]) 
         resultIpfsHash   = str(sys.argv[4]) 
         storageID        = int(sys.argv[5]) 
-        endTime          = int(sys.argv[6]) 
-    #}
+        endTime          = int(sys.argv[6])
+        usedBandwidthMB  = int(sys.argv[7])
     else: # Dummy call        
         jobKey           = '231037324805864425899587012070500513653' 
         index            = 0
@@ -33,6 +34,6 @@ if __name__ == '__main__': #{
         resultIpfsHash   = 'QmRsaBEGcqxQcJbBxCi1LN9iz5bDAGDWR6Hx7ZvWqgqmdR'
         storageID        = 0
         endTime          = 1128590
+        usedBandwidthMB  = 100
     
-    print(receiptCheck(jobKey, index, jobRunTimeMinute, resultIpfsHash, storageID, endTime))
-#}
+    print(receiptCheck(jobKey, index, jobRunTimeMinute, resultIpfsHash, storageID, endTime, usedBandwidthMB))
