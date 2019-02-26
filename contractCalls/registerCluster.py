cfcfbcccfb3c56511a8b903b071692ed1821e6f0
avatar-lavventura <alper.alimoglu@gmail.com>
2019-02-26 13:50:16 +0300

    update

--- contractCalls/registerCluster.py
+++ contractCalls/registerCluster.py
@@ -16,12 +16,14 @@ CLUSTER_ID  = web3.toChecksumAddress(os.getenv("CLUSTER_ID"))
 if __name__ == '__main__':
     # USER Inputs----------------------------------------------------------------
     account           = CLUSTER_ID # web3.eth.accounts[0]  # Cluster's Ethereum Address
-    coreNumber        = 2 
+    availableCoreNum  = 2 
     clusterEmail      = "alper.alimoglu@gmail.com" 
     federationCloudId = "5f0db7e4-3078-4988-8fa5-f066984a8a97@b2drop.eudat.eu" 
     miniLockId        = "9VZyJy1gRFJfdDtAjRitqmjSxPjSAjBR6BxH59UeNgKzQ" 
     priceCoreMin      = 100
     priceDataTransfer = 10
+    priceStorage      = 10
+    priceCache        = 10
     ipfsAddress       = "/ip4/79.123.177.145/tcp/4001/ipfs/QmWmZQnb8xh3gHf9ZFmVQC4mLEav3Uht5kHJxZtixG3rsf"
     # whisperPubKey     = "0x04aec8867369cd4b38ce7c212a6de9b3aceac4303d05e54d0da5991194c1e28d36361e4859b64eaad1f95951d2168e53d46f3620b1d4d2913dbf306437c62683a6"
     # ----------------------------------------------------------------------------	
@@ -39,7 +41,17 @@ if __name__ == '__main__':
             sys.exit()
 
     if len(federationCloudId) < 128 and len(clusterEmail) < 128 and (len(miniLockId) == 0 or len(miniLockId) == 45):
-        tx_hash = eBlocBroker.transact({"from":account, "gas": 4500000}).registerCluster(coreNumber, clusterEmail, federationCloudId, miniLockId, priceCoreMin, priceDataTransfer, ipfsAddress, whisperPubKey) 
+        tx_hash = eBlocBroker.transact({"from":account,
+                                        "gas": 4500000}).registerCluster(clusterEmail,
+                                                                         federationCloudId,
+                                                                         miniLockId,
+                                                                         availableCoreNum,
+                                                                         priceCoreMin,
+                                                                         priceDataTransfer,
+                                                                         priceStorage,
+                                                                         priceCache,
+                                                                         ipfsAddress,
+                                                                         whisperPubKey) 
         print('Tx_hash: ' + tx_hash.hex()) 
 
     # os.environ['ipfs'] = ipfs 
