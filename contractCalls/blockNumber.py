13855449e065008b81f4f4a0c30c369a747b6108
avatar-lavventura <alper.alimoglu@gmail.com>
2019-03-09 15:21:27 +0300

    update

--- contractCalls/blockNumber.py
+++ contractCalls/blockNumber.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-def blockNumber(web3=None): #{
-    if web3 != None:
+def blockNumber(web3=None):
+    if web3 is None:
+        import sys, os
+        sys.path.insert(1, os.path.join(sys.path[0], '..'))
+        from imports import getWeb3
+
+        web3 = getWeb3()
+        print(web3)
         return str(web3.eth.blockNumber).replace("\n", "")
     else:
-        import sys, os 
-        sys.path.insert(1, os.path.join(sys.path[0], '..')) 
-        from imports import getWeb3
-        
-        web3 = getWeb3() 
-        return str(web3.eth.blockNumber).replace("\n", "") 
-#}
+        return str(web3.eth.blockNumber).replace("\n", "")
 
-if __name__ == '__main__': #{
+if __name__ == '__main__':
     print(blockNumber())
-#}
