2e2d766fd8882af49aa05087b2e2c5475e70cee4
avatar-lavventura <alper.alimoglu@gmail.com>
2019-03-12 12:46:22 +0300

    new contract update

--- generateTest/eudatNasa/generateEudat.py
+++ generateTest/eudatNasa/generateEudat.py
@@ -1,25 +1,37 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import owncloud, hashlib, getpass, os, time, math, datetime, random, sys
 from os.path import expanduser
 from random import randint
 
-path = os.getcwd();
+path = os.getcwd()
 home = expanduser("~")
-os.environ['path'] = path;
 
-def contractCall(val):
-      return os.popen( val + "| node").read().replace("\n", "").replace(" ", "");
-    
-counter = 0;
-itemsToScan = 1;
+sys.path.insert(0, home + '/eB')
+# sys.path.insert(0, home + '/eB/contractCalls')
+
+from lib_owncloud import singleFolderShare
+from lib_owncloud import eudatInitializeFolder
+
+os.environ['path'] = path
+
+# Login to EUDAT account----------------------------------------
+f = open(home + '/TESTS/password.txt', 'r') # Password read from the file.
+password = f.read().replace("\n", "").replace(" ", "")
+f.close()
+oc = owncloud.Client('https://b2drop.eudat.eu/')
+oc.login('059ab6ba-4030-48bb-b81b-12115f531296', password)
+#---------------------------------------------------------------
+
+counter = 0 #150
+itemsToScan = 1
 hashesFile = open(path + '/hashOutput.txt', 'w+')
-commentStr = "QmQANSjxQaziHPdMuj37LC53j65cVtXXwQYvu8GxJCPFJE"; # Dummy hash string
+# commentStr = "QmQANSjxQaziHPdMuj37LC53j65cVtXXwQYvu8GxJCPFJE" # Dummy hash string
 
 with open(path + "/nasa.txt") as test:
     for line in test:
         f = open(path + '/ipfs/run.sh', 'w+')
-        lineIn = line.split(" ");
+        lineIn = line.split(" ")
 
         if ((int(lineIn[1]) - int(lineIn[0])) > 60 ):
            print( "Time to take in seconds: "  + str(int(lineIn[1]) - int(lineIn[0])) )
@@ -28,36 +40,30 @@ with open(path + "/nasa.txt") as test:
 
            with open(path + "/ipfs/run_temp.sh") as ff:
               for line in ff:
-                 f.write(line);
+                 f.write(line)                           
 
-           commentStr = str(random.getrandbits(128)) # Generates Random Hash
-           print(commentStr)
+           tarHash = eudatInitializeFolder(path +  '/ipfs', oc)
+           time.sleep(1)
+           print(singleFolderShare(tarHash, oc))
 
-           f.write("sleep " + str(int(lineIn[1]) - int(lineIn[0])) + "\n");
-           f.write("#" + commentStr + "\n"); # Add random line to create different hash
-           f.write("echo completed " + str(int(lineIn[1]) - int(lineIn[0])) + " > " + commentStr + ".txt\n"); #add random line to create different hash.
-           f.write("echo completed " + str(int(lineIn[1]) - int(lineIn[0])) + " > completed.txt\n" ); #add random line to create different hash.
-           f.close();
+           f.write("sleep " + str(int(lineIn[1]) - int(lineIn[0])) + "\n")
+           f.write("#" + tarHash + "\n") # Add random line to create different hash
+           f.write("echo completed " + str(int(lineIn[1]) - int(lineIn[0])) + " > " + tarHash + ".txt\n") #add random line to create different hash.
+           f.write("echo completed " + str(int(lineIn[1]) - int(lineIn[0])) + " > completed.txt\n" ) #add random line to create different hash.
+           f.close()
 
-           #ipfsHash = os.popen( 'IPFS_PATH="/home/prc/.ipfs"; export IPFS_PATH; ipfs add -r /home/prc/testIpfs/ipfs' ).read();
-           #ipfsHash = ipfsHash.split("\n");
-           #commentStr = ipfsHash[len(ipfsHash) - 2].split(" ")[1];
-           #print( "HASH: " + commentStr ); # lineNumber -> hash olarak kaydet.
 
-           copyIntoSharePath = home + "/oc" + "/" + commentStr;
-           os.environ['copyIntoSharePath'] = copyIntoSharePath;
-
-           if not os.path.isdir(copyIntoSharePath):
-              os.makedirs(copyIntoSharePath)
-
-           os.popen("cp -a " + path +  "/ipfs/* $copyIntoSharePath")
-
-           hashesFile.write(commentStr + " " + str(int(lineIn[1]) - int(lineIn[0])) + " " + str(int(lineIn[2])) +"\n"); ##
+           #ipfsHash = os.popen( 'IPFS_PATH="/home/prc/.ipfs" export IPFS_PATH ipfs add -r /home/prc/testIpfs/ipfs' ).read()
+           #ipfsHash = ipfsHash.split("\n")
+           #tarHash = ipfsHash[len(ipfsHash) - 2].split(" ")[1]
+           #print( "HASH: " + tarHash ) # lineNumber -> hash olarak kaydet.
+                     
+           hashesFile.write(tarHash + " " + str(int(lineIn[1]) - int(lineIn[0])) + " " + str(int(lineIn[2])) +"\n") 
            if (counter == itemsToScan):
-              break;
+              break
            print(counter)
-           counter += 1;
-hashesFile.close();
+           counter += 1
+hashesFile.close()
 
 print('\nFolders are created. Sharing files now...')
 print(os.popen('python $path/shareOwnCloud.py').read())
--- test/authenticateUsersPrc.py
+++ test/authenticateUsersPrc.py
@@ -13,12 +13,19 @@ from imports import connectEblocBroker
 from imports import getWeb3
 
 sys.path.insert(0, home + '/eBlocBroker/contractCalls')
-from registerUser import registerUser
+from authenticateORCID import authenticateORCID
 
 web3        = getWeb3()
 eBlocBroker = connectEblocBroker(web3)
 # -------------------------------------------------------
+orcID = '0000-0001-7642-0552'
 
+accounts= ["0x90Eb5E1ADEe3816c85902FA50a240341Fa7d90f5", "0x289C3D804a3B5bEB9C5A542AD2711541cbb1572c", "0xb1FAf341B5c05AfAD8C019d7118f3B2a635b88ED", "0x7A4718561D4B6c06214093Bd22EA074b0C398F5F", "0xaDD445Ce732c425998e3A0c4009fA5DD5E81c995", "0x8A22243ED31966B2DF91CcDd99d1181Fdf6fe546", "0x75e96A39Dd6267A054EC8E0C9942306531d00EA8", "0x6F53FBB4F882C1969271Ad20E2eB68Df0642Dd79", "0x948f9E469a18B72cf960AE51B320503B3b6A603e", "0x496acA2bE0694A1137B05cE7fFDcd982bFb53FAD"];
+
+for i in range(0, len(accounts)):
+    print(authenticateORCID(accounts[i], orcID))
+
+'''
 userEmail         = "aalimog1@binghamton.edu"
 federationCloudID = "3d8e2dc2-b855-1036-807f-9dbd8c6b1579"
 miniLockID        = "jj2Fn8St9tzLeErBiXA6oiZatnDwJ2YrnLY3Uyn4msD8k"
@@ -26,16 +33,8 @@ orcid             = "0000-0001-7642-0552"
 ipfsAddress       = "/ip4/193.140.197.95/tcp/3002/ipfs/QmSdnexZEQGKuj31PqrRP7XNZ4wvKMZWasvhqDYc9Y5G3C"
 githubUserName    = "eBloc"
 
-accoutLength = 10
-for i in range(0, accoutLength):
+for i in range(0, 10):
     accountID = str(i)
-    account = web3.eth.accounts[i]
     tx = registerUser(accountID, userEmail, federationCloudID, miniLockID, ipfsAddress, orcid, githubUserName, eBlocBroker, web3)
-    print(account + ' | ' + str(i) + ' | ' +  tx)
-print('')
-print('[', end='')
-for i in range(0, accoutLength):
-    print("\"" + web3.eth.accounts[i] + "\"", end='')
-    if i != accoutLength-1:
-        print(', ', end='')
-print(']')
+    print(str(i) + ' | ' + tx)
+'''
