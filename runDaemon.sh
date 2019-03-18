fdedc5a02781c937b36d12e974683938a8ae19e5
avatar-lavventura <alper.alimoglu@gmail.com>
2019-03-18 14:33:04 +0300

    update

--- runDaemon.sh
+++ runDaemon.sh
@@ -8,13 +8,10 @@ sudo printf ''
 if [[ "$EUID" -ne 0 ]]; then
     . venv/bin/activate;
     nohup python3 -Bu Driver.py >> $HOME/.eBlocBroker/clusterDriver.out 2>&1 &
-    sudo tail -f $HOME/.eBlocBroker/clusterDriver.out
+    tail -f $HOME/.eBlocBroker/clusterDriver.out
 else
     echo "This script must be run as non-root. Please run without 'sudo'." 
 fi
 
 #gopath=$(go env | grep 'GOPATH' | cut -d "=" -f 2 | tr -d '"');
 #export PATH=$PATH:$gopath/bin;
-
-# sudo su -c ". venv/bin/activate && nohup python -Bu Driver.py >> $HOME/.eBlocBroker/clusterDriver.out 2>&1 &"
-# sudo tail -f $HOME/.eBlocBroker/clusterDriver.out
