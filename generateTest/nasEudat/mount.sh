a17196b7095e18ba6759d3923e7f1aacdb5b0a1a
avatar-lavventura <alper.alimoglu@gmail.com>
2019-03-20 09:27:33 +0300

    update

--- generateTest/nasEudat/mount.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p oc/
-sudo umount oc/
-sudo mount.davfs https://b2drop.eudat.eu/remote.php/webdav/ oc/ # prc-95 username: alper.alimoglu@boun.edu.tr
--- generateTest/testNasEudat/README.txt
+++
@@ -1,5 +0,0 @@
-Do:
-sudo mount.davfs https://b2drop.eudat.eu/remote.php/webdav/ oc
-sudo umount.davfs oc
-
-User: alper.alimoglu@boun.edu.tr
