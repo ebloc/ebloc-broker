74b58532cfba0de21a79cb452cc4f2dede0f12a8
avatar-lavventura <alper.alimoglu@gmail.com>
2019-03-23 16:54:36 +0300

    update

--- user.sh
+++ user.sh
@@ -7,13 +7,13 @@ BASEDIR=$2
 #USER_ADDRESS="dummy"
 #BASEDIR="/var/eBlocBroker"
 
-SLURMUSER="alper"
+SLURMUSER="netlab"
 USERNAME=$(echo -n $USER_ADDRESS | md5sum | head -c-4) # Convert Ethereum User Address into 32-bits
 
-# Force to add
-sacctmgr remove user where user=$USERNAME --immediate
-sacctmgr add account $USERNAME --immediate
-sacctmgr create user $USERNAME defaultaccount=$USERNAME adminlevel=[None] --immediate    
+## Force to add
+#sacctmgr remove user where user=$USERNAME --immediate
+#sacctmgr add account $USERNAME --immediate
+#sacctmgr create user $USERNAME defaultaccount=$USERNAME adminlevel=[None] --immediate    
 
 if ! id -u $USERNAME > /dev/null 2>&1; then
     sudo useradd -d $BASEDIR/$USERNAME -m $USERNAME
@@ -23,7 +23,8 @@ if ! id -u $USERNAME > /dev/null 2>&1; then
     sudo chmod 700 $BASEDIR/$USERNAME # Block others and people in the same group to do read/write/execute
     sudo setfacl -R -m user:$USERNAME:rwx  $BASEDIR/$USERNAME #Give read/write/execute access to USER1 on give folder.
     sudo setfacl -R -m user:$SLURMUSER:rwx $BASEDIR/$USERNAME #Give read/write/execute access to USER2 on give folder.
-    
+
+    sacctmgr remove user where user=$USERNAME --immediate
     sacctmgr add account $USERNAME --immediate
     sacctmgr create user $USERNAME defaultaccount=$USERNAME adminlevel=[None] --immediate    
 else
@@ -41,7 +42,7 @@ else
 
 	echo $USER_ADDRESS / $USERNAME 'is created.'
 
-	# Force to add
+	## Force to add
 	sacctmgr remove user where user=$USERNAME --immediate
 	sacctmgr add account $USERNAME --immediate
 	sacctmgr create user $USERNAME defaultaccount=$USERNAME adminlevel=[None] --immediate    
