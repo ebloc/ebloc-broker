62262b47971aac0d9e3c523bcfbbd4f2b8bebb37
avatar-lavventura <alper.alimoglu@gmail.com>
2019-04-11 12:05:00 +0300

    getJob through GitHub is updated

--- contract/run.sh
+++ contract/run.sh
@@ -3,6 +3,8 @@
 # brew switch solidity 0.4.17
 
 # source ~/envname/bin/activate
+source $HOME/myapp/bin/activate
+
 rm -rf tests/__pycache__
 export TESTRPC_GAS_LIMIT=8000000
 py.test --capture=fd tests/test.py -s --disable-pytest-warnings
