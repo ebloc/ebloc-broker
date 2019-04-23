6244fc44d2ecbba30af1962258c6bb89b1c4a1fc
avatar-lavventura <alper.alimoglu@gmail.com>
2019-04-23 23:26:45 +0300

    Lib/receiptCheck uint => uint32 updated

--- workflow/contracts/Greeter.sol
+++ workflow/contracts/Greeter.sol
@@ -24,4 +24,34 @@ contract Greeter {
     function greet() public constant returns (bytes32) {
         return value;
     }
+    
+    // --------
+    uint variable;
+    uint doo;
+
+    function setCharacter(uint variable_) 
+        external 
+    {
+        variable = variable_;
+	doo  = uint32(-1);
+	doo |= 10 << 32;
+	doo |= 20 << 64; 
+	doo |= 11 << 32;
+
+	
+    }
+
+    function getDoo()
+	public view returns (int32, uint32, uint32, uint){
+	
+	return (int32(doo), uint32(doo >> 32), uint32(doo >> 64),doo);
+
+    }
+	    
+    function getVariables() 
+        public view
+    returns(uint128 variable1, uint128 variable2) {
+        variable1 = uint64(variable);
+        variable2 = uint128(variable>>128);
+    }
 }
