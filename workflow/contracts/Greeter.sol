5123d9c02b27e79fdebcddb50354d5cb6994a914
avatar-lavventura <alper.alimoglu@gmail.com>
2019-04-24 20:02:34 +0300

    Lib update.

--- workflow/contracts/Greeter.sol
+++ workflow/contracts/Greeter.sol
@@ -28,23 +28,46 @@ contract Greeter {
     // --------
     uint variable;
     uint doo;
+    uint [] v;
 
+    function array(uint [] variable_) {
+	v = variable_;
+    }
+
+    function getMoo()
+	public view returns(uint []) {
+	return v;
+    }
+
+    
     function setCharacter(uint variable_) 
         external 
     {
-        variable = variable_;
-	doo  = uint32(-1);
-	doo |= 10 << 32;
-	doo |= 20 << 64; 
-	doo |= 11 << 32;
+	uint32 vari = 4294967295;
+	uint32 var2 = 12;
 
+	    
+        variable = variable_;
+	doo |= uint32(-1) + uint(uint(vari) << 32) + uint(uint(var2) << 64);
+	doo &= 115792089237316195423570985008687907853269984665640564039439137263843715055615;
+	doo |= uint(100) << 32;
+ 
+	//doo |= uint32(100) << 32;
 	
+	
+	
+	doo = uint(uint(vari) << 64);
+	//doo |= 20 << 64; 
+	//doo |= 11 << 32;	
+	//
+	//doo |= uint32(-1);	
     }
 
     function getDoo()
 	public view returns (int32, uint32, uint32, uint){
-	
-	return (int32(doo), uint32(doo >> 32), uint32(doo >> 64),doo);
+
+	uint koo = 115792089237316195423570985008687907853269984665640564039457584007913129639935 ^ 79228162495817593519834398720;
+	return (int32(doo), uint32(doo >> 32), uint32(doo >> 64), koo);
 
     }
 	    
