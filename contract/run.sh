90a75748e3a6bf83a0c9224e0dd31f487eb81b70
avatar-lavventura <alper.alimoglu@gmail.com>
2019-04-29 21:57:31 +0300

    job converted into workflow format as receiving and storing it as an array

--- contract/run.sh
+++ contract/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# brew switch solidity 0.4.17
-
-# source ~/envname/bin/activate
-source $HOME/myapp/bin/activate
-
-rm -rf tests/__pycache__
-export TESTRPC_GAS_LIMIT=8000000
-py.test --capture=fd tests/test.py -s --disable-pytest-warnings
+rm contracts/.\#Lib.sol
+brownie compile
+brownie test
--- workflow/run.sh
+++ workflow/run.sh
@@ -2,7 +2,6 @@
 
 # brew switch solidity 0.4.17
 
-source $HOME/myapp/bin/activate
 rm -rf tests/__pycache__
 export TESTRPC_GAS_LIMIT=8000000
 py.test --capture=fd tests/test.py -s --disable-pytest-warnings
