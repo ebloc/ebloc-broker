e9283ead3a3c07168ae4274633c597faf3d8fab4
alper <alper.alimoglu@gmail.com>
2020-03-21 23:49:45 +0300

    update

---
+++ dag/example.py
@@ -0,0 +1,23 @@
+import sys
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+g = nx.Graph()
+G = nx.DiGraph()
+
+G.add_edge("A", "B", weight=10)
+G.add_edge("A", "C", weight=0)
+# 3 depends on 1,2
+G.add_edge("B", "D", weight=0)
+G.add_edge("C", "D", weight=0)
+# 4 depends on 1
+G.add_edge("B", "job4", weight=0)
+# 5 depends on 3 and 4
+G.add_edge("C", "E", weight=0)
+G.add_edge("D", "E", weight=10)
+
+nx.nx_pydot.write_dot(G, "job.dot")  # Saves DAG into job.dot file
+
+nx.draw(G, with_labels=True)
+plt.savefig("labels.png")
---
+++ dag/read_dag.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import random
+import sys
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+job_ids = {}
+
+
+def dependencyJob(i):
+    if len(set(G.predecessors(i))) == 0:
+        job_id = notDependentSubmitJob(i)
+        job_ids[i] = job_id
+        print("job_id: " + str(job_id))
+    else:
+        job_id = dependentSubmitJob(i, list(G.predecessors(i)))
+        job_ids[i] = job_id
+        print("job_id: " + str(job_id))
+        print(list(G.predecessors(i)))
+        # sys.exit()
+
+
+def notDependentSubmitJob(i):
+    print("sbatch " + i + ".sh")
+    return random.randint(1, 101)
+
+
+def dependentSubmitJob(i, predecessors):
+    if len(predecessors) == 1:
+        if not predecessors[0] in job_ids:  # If the required job is not submitted to Slurm, recursive call
+            dependencyJob(predecessors[0])
+
+        print("sbatch --dependency=afterok:" + str(job_ids[predecessors[0]]) + " " + i + ".sh")
+        return random.randint(1, 101)
+    else:
+        job_id_str = ""
+
+        for j in predecessors:
+            if not j in job_ids:  # If the required job is not submitted to Slurm, recursive call
+                dependencyJob(j)
+
+            job_id_str += str(job_ids[j]) + ":"
+
+        job_id_str = job_id_str[:-1]
+        # print(job_id_str)
+        print("sbatch --dependency=afterok:" + job_id_str + " " + i + ".sh")
+        return random.randint(1, 101)
+
+
+G = nx.drawing.nx_pydot.read_dot("job.dot")
+
+print("List of nodes:")
+print(list(G.nodes))
+
+for i in list(G.nodes):
+    # print(i)
+    # print(len(set(G.successors(i))))
+    if not i in job_ids:
+        dependencyJob(i)
+
+
+for i in list(G.nodes):
+    print(i + " " + str(job_ids[i]))
+
+# jid4=$(sbatch  --dependency=afterany:$jid2:$jid3 job4.sh
