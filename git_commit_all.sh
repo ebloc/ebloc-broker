8692cf9477298c1a3745428c2ffa16bac38843bd
avatar-lavventura <alper.alimoglu@gmail.com>
2020-03-29 19:42:53 +0300

    is_dir() function is carried into lib.py function.

--- git_commit_all.sh
+++ git_commit_all.sh
@@ -3,11 +3,12 @@
 ./clean.sh
 git add -A .
 
-str=$1
-if [ ! -z "$str" -a "$str" != " " ]; then
-    git commit -m $str
-else
-    git commit -m "update"
-fi
-
-git push
+git commit
+# str=$1
+#if [ ! -z "$str" -a "$str" != " " ]; then
+#    git commit -m $str
+#else
+#    git commit -m "update"
+#fi
+#
+#git push
