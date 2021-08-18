03029701fcfaa2b99f4a9113491d3ff41152b3ac
avatar-lavventura <alper.alimoglu@gmail.com>
2021-08-18 04:02:24 -0700

    Work on bloxberg
    - Remove "color=" for log() function

--- broker/eblocbroker/get_job_storage_time.py
+++ broker/eblocbroker/get_job_storage_time.py
@@ -7,14 +7,14 @@ import broker.eblocbroker.Contract as Contract
 if __name__ == "__main__":
     Ebb = Contract.eblocbroker
     if len(sys.argv) == 3:
-        providerAddress = str(sys.argv[1])
-        sourceCodeHash = str(sys.argv[2])
+        provider_addr = str(sys.argv[1])
+        source_code_hash = str(sys.argv[2])
     else:
-        providerAddress = "0x57b60037b82154ec7149142c606ba024fbb0f991"
-        sourceCodeHash = "acfd2fd8a1e9ccf031db0a93a861f6eb"
+        provider_addr = "0x57b60037b82154ec7149142c606ba024fbb0f991"
+        source_code_hash = "acfd2fd8a1e9ccf031db0a93a861f6eb"
 
-    receivedBlockNum, storageTime = Ebb.getJobStorageTime(providerAddress, sourceCodeHash)
+    received_block_num, storage_time = Ebb.getJobStorageTime(provider_addr, source_code_hash)
     print(
-        f"receivedBlockNum={receivedBlockNum}; storageTime={storageTime};"
-        f" endBlockTime={receivedBlockNum + storageTime * 240}"
+        f"received_block_num={received_block_num}; storage_time={storage_time};"
+        f" endBlockTime={received_block_num + storage_time * 240}"
     )
