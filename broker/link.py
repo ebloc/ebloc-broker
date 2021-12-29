66ec84c5ca6445e00554cb5a5f93106459313285
avatar-lavventura <alper.alimoglu@gmail.com>
2021-12-29 17:18:25 +0000

    Fix gdrive upload and download.
    There was a bug due to files inside emptied trash also be listed. trash=false
    added into the gdrive query to fix it.

--- broker/link.py
+++ broker/link.py
@@ -160,4 +160,4 @@ def check_linked_data(paths_from, path_to, is_pass=False):
 if __name__ == "__main__":
     path_from = env.HOME / "test_eblocbroker" / "test_data" / "base" / "data"
     path_to = env.LINK_PATH / "base" / "data_link"
-    check_linked_data(path_from, path_to, is_continue=True)
+    check_linked_data(path_from, path_to, is_pass=True)
