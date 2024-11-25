4d39d467eed581a133e2135be964345ac0da3055
avatar-lavventura <alper.alimoglu@gmail.com>
2024-11-25 18:44:29 +0000

    Fix some bugs, minor improvements, and more

--- docs/run.sh
+++ docs/run.sh
@@ -4,7 +4,8 @@
 IP=127.0.0.1
 PORT=8000
 # sphinx-autobuild . _build_html --host $IP --port $PORT
-sphinx-autobuild . -aE _build_html
+# sphinx-autobuild . -aE _build_html
+sphinx-autobuild . _build/html -b html --host 0.0.0.0 --port 8000
 
 # https://stackoverflow.com/a/20484362/2402577
 # watchmedo shell-command \
